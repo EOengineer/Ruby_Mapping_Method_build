e73825479d8698c25e21250b8a4c62b3bf91cf3e
EOengineer <EOengineer@gmail.com>
2014-05-07 19:11:43 -0400

    refactored as a class method

--- map_method.rb
+++ map_method.rb
@@ -1,15 +1,17 @@
 require 'pry'
 
-def mapping(array)
-  mapped_array = Array.new
-  remaining_iterations = array.size
-  index = 0
-  until remaining_iterations <= 0
-    mapped_array << array[index] if array[index] > 5
-    index += 1
-    remaining_iterations -= 1
+class Array
+  def mapping
+    mapped_array = Array.new
+    stop_iterating = self.size
+    index = 0
+    until index >= stop_iterating
+      mapped_array << self[index] if self[index] > 5
+      index += 1
+    end
+    mapped_array
   end
-  mapped_array
 end
 
-puts mapping(1.upto(10).to_a)
+array = (1..10).to_a
+puts array.mapping
