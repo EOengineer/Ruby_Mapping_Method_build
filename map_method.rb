0529e2265e01be2169c1f5328184033bca3a579e
EOengineer <EOengineer@gmail.com>
2014-05-07 14:34:41 -0400

    altered variable names for readability

--- map_method.rb
+++ map_method.rb
@@ -2,12 +2,12 @@ require 'pry'
 
 def mapping(array)
   mapped_array = Array.new
-  counter = array.size
+  remaining_iterations = array.size
   index = 0
-  until counter <= 0
+  until remaining_iterations <= 0
     mapped_array << array[index] if array[index] > 5
     index += 1
-    counter -= 1
+    remaining_iterations -= 1
   end
   mapped_array
 end
